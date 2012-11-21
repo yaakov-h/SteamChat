44d9d5377a744561e05cd48f0f8b9494b768c11e
Yaakov <pubgit@codingrange.com>
2012-11-22 10:22:51 +1100

    Initial Commit

---
+++ SteamChat/SCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCAppDelegate.h
+//  SteamChat
+//
+//  Created by Yaakov on 22/11/12.
+//  Copyright (c) 2012 OpenSteamWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SteamChat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SteamChat
+//
+//  Created by Yaakov on 22/11/12.
+//  Copyright (c) 2012 OpenSteamWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([SCAppDelegate class]));
+	}
+}
